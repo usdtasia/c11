4211fd8c847652f8dbae4fcbc86e5884362db1d6
Lucoyan <h.sargsyan@aumify.io>
2024-12-24 14:44:36 +0400

    feature: added new rate column to orders history table

--- src/components/history/HistoryLine.jsx
+++ src/components/history/HistoryLine.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { format } from "date-fns";
 import { Link } from "react-router-dom";
 
-function HistoryLine({ createdAt, from, to, status, darker, transactionId }) {
+function HistoryLine({ createdAt, from, rate, to, status, darker, transactionId }) {
   const formattedDate = format(new Date(createdAt), "dd.MM.yyyy HH:mm");
   const statusMap = {
     0: "created",
@@ -12,29 +12,32 @@ function HistoryLine({ createdAt, from, to, status, darker, transactionId }) {
     4: "paid",
   };
   return (
-    <Link
-      to={`/zayavka/${transactionId}`}
-      className={`table__content_line ${darker}`}
-    >
-      <p className="table__content_line_el table__content_line_el_date">
-        {formattedDate}
-      </p>
-      <p className="table__content_line_el history__blue_font table__content_line_el_from">
-        {from.name}
-      </p>
-      <p className="table__content_line_el table__content_line_el_value">
-        {from.value.toFixed(2)}
-      </p>
-      <p className="table__content_line_el history__blue_font">{to.name}</p>
-      <p className="table__content_line_el table__content_line_el_value">
-        {to.value.toFixed(2)}
-      </p>
-      <p
-        className={`table__content_line_el  history__green_font ${statusMap[status]}`}
+      <Link
+          to={`/zayavka/${transactionId}`}
+          className={`table__content_line ${darker}`}
       >
-        {statusMap[status]}
-      </p>
-    </Link>
+        <p className="table__content_line_el table__content_line_el_date">
+          {formattedDate}
+        </p>
+        <p className="table__content_line_el history__blue_font table__content_line_el_from">
+          {from.name}
+        </p>
+        <p className="table__content_line_el table__content_line_el_value">
+          {from.value.toFixed(2)}
+        </p>
+        <p className="table__content_line_el history__blue_font">{to.name}</p>
+        <p className="table__content_line_el table__content_line_el_value">
+          {to.value.toFixed(2)}
+        </p>
+        <p className="table__content_line_el table__content_line_el_value">
+          {rate}
+        </p>
+        <p
+            className={`table__content_line_el  history__green_font ${statusMap[status]}`}
+        >
+          {statusMap[status]}
+        </p>
+      </Link>
   );
 }
 
--- src/components/history/TableStaticLine.jsx
+++ src/components/history/TableStaticLine.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 
 function TableStaticLine() {
-  const { t } = useTranslation();
-  return (
-    <div className="table__static_line">
-      <p className="table__static_line_el">{t("History_1")}</p>
-      <p className="table__static_line_el">{t("History_2")}</p>
-      <p className="table__static_line_el">{t("History_3")}</p>
-      <p className="table__static_line_el">{t("History_4")}</p>
-      <p className="table__static_line_el table__static_line_el_amount">
-        {t("History_3")}
-      </p>
-      <p className="table__static_line_el">{t("History_5")}</p>
-    </div>
-  );
+    const {t} = useTranslation();
+    return (
+        <div className="table__static_line">
+            <p className="table__static_line_el">{t("History_1")}</p>
+            <p className="table__static_line_el">{t("History_2")}</p>
+            <p className="table__static_line_el">{t("History_3")}</p>
+            <p className="table__static_line_el">{t("History_4")}</p>
+            <p className="table__static_line_el table__static_line_el_amount">
+                {t("History_3")}
+            </p>
+            <p className="table__static_line_el">
+                {t("History_6")}
+            </p>
+            <p className="table__static_line_el">{t("History_5")}</p>
+        </div>
+    );
 }
 
 export default TableStaticLine;
